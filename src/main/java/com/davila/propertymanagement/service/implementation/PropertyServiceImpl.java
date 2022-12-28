8dae290a26e7dde3072f7e748264f9b92299ad24
mirasoldavila13 <mirasoldavila13@aim.com>
2022-12-28 15:04:35 -0800

    created implementation class

---
+++ src/main/java/com/davila/propertymanagement/service/implementation/PropertyServiceImpl.java
@@ -0,0 +1,14 @@
+package com.davila.propertymanagement.service.implementation;
+
+import com.davila.propertymanagement.dto.PropertyDTO;
+import com.davila.propertymanagement.service.PropertyService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PropertyServiceImpl implements PropertyService {
+
+    @Override
+    public PropertyDTO saveProperty(PropertyDTO propertyDTO) {
+        return null;
+    }
+}
