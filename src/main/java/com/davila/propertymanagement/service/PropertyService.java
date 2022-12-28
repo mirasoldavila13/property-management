eb814f089cc7249d17c999e6a0f0bf42a39843e3
mirasoldavila13 <mirasoldavila13@aim.com>
2022-12-28 13:33:38 -0800

    created interface

--- src/main/java/com/davila/propertymanagement/service/PropertyService.java
+++ src/main/java/com/davila/propertymanagement/service/PropertyService.java
@@ -1,4 +1,8 @@
 package com.davila.propertymanagement.service;
 
+import com.davila.propertymanagement.dto.PropertyDTO;
+
 public interface PropertyService {
+    public PropertyDTO saveProperty(PropertyDTO propertyDTO);
+
 }
