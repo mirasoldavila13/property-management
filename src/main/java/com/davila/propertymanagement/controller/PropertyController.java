d8584815f10c058597cab3e954a149060c46a9b5
mirasoldavila13 <mirasoldavila13@aim.com>
2022-12-28 15:27:16 -0800

    automatic dependency injection.

--- src/main/java/com/davila/propertymanagement/controller/PropertyController.java
+++ src/main/java/com/davila/propertymanagement/controller/PropertyController.java
@@ -1,12 +1,19 @@
 package com.davila.propertymanagement.controller;
 
 import com.davila.propertymanagement.dto.PropertyDTO;
+import com.davila.propertymanagement.service.PropertyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
 public class PropertyController {
-    
+
+    @Autowired
+    private PropertyService propertyService;
+
+    //RESTFUL API is just mapping of a url to a java class function
+    //http://localhost:8089/api/v1/properties/hello
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello";
@@ -14,6 +21,7 @@ public class PropertyController {
 
     @PostMapping("/properties")
     public PropertyDTO saveProperty(@RequestBody PropertyDTO propertyDTO){
+        propertyService.saveProperty(propertyDTO);
         System.out.println(propertyDTO);
         return propertyDTO;
     }
