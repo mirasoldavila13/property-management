1ab3d1285335efb8782e532217185c6029159970
mirasoldavila13 <mirasoldavila13@aim.com>
2022-12-31 23:04:27 -0800

    Reading Custom Config Properties Inside Java Class

--- src/main/java/com/davila/propertymanagement/controller/PropertyController.java
+++ src/main/java/com/davila/propertymanagement/controller/PropertyController.java
@@ -3,6 +3,7 @@ package com.davila.propertymanagement.controller;
 import com.davila.propertymanagement.dto.PropertyDTO;
 import com.davila.propertymanagement.service.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +13,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1")
 public class PropertyController {
+    @Value("${pms.dummy}:")
+    private String dummy;
 
+    @Value("${spring.datasource.url}:")
+    private String dbUrl;
+    
     @Autowired
     private PropertyService propertyService;
 
@@ -32,6 +38,8 @@ public class PropertyController {
 
     @GetMapping("/properties")
     public ResponseEntity<List<PropertyDTO>> getAllProperties(){
+       System.out.println(dummy);
+       System.out.println(dbUrl);
        List<PropertyDTO> propertyList =  propertyService.getAllProperties();
        ResponseEntity<List<PropertyDTO>> responseEntity = new ResponseEntity<>(propertyList, HttpStatus.OK);
        return responseEntity;
