2f09cb8b3b09a22ecf539523dcc5a32d079b3629
mirasoldavila13 <mirasoldavila13@aim.com>
2023-01-01 12:15:58 -0800

    Creating UserEntity and UserRepository

---
+++ src/main/java/com/davila/propertymanagement/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.davila.propertymanagement.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<UserRepository, Long> {
+
+}
