4cbd604190b637e5f9990120cf8f7f33af380a3b
mirasoldavila13 <mirasoldavila13@aim.com>
2023-01-02 12:40:29 -0800

    working on User Service Impl

--- src/main/java/com/davila/propertymanagement/service/implementation/UserServiceImpl.java
+++ src/main/java/com/davila/propertymanagement/service/implementation/UserServiceImpl.java
@@ -1,14 +1,19 @@
 package com.davila.propertymanagement.service.implementation;
 
 import com.davila.propertymanagement.dto.UserDTO;
+import com.davila.propertymanagement.repository.UserRepository;
 import com.davila.propertymanagement.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public UserDTO register(UserDTO userDTO) {
+
         return null;
     }
 
