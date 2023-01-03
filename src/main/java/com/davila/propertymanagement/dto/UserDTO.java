a61dec0d82323ecf1aadf40af64dc04365c203d8
mirasoldavila13 <mirasoldavila13@aim.com>
2023-01-03 14:40:06 -0800

    JSONIgnore and JsonInclude Annotation

--- src/main/java/com/davila/propertymanagement/dto/UserDTO.java
+++ src/main/java/com/davila/propertymanagement/dto/UserDTO.java
@@ -1,10 +1,15 @@
 package com.davila.propertymanagement.dto;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDTO {
 
     private Long id;
