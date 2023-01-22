afe9a8625462af5d46f7f8fc1baa5d5c4e3c0f38
mirasoldavila13 <mirasoldavila13@aim.com>
2023-01-21 21:20:21 -0800

    Monitoring with Spring Boot Actuator

--- src/main/java/com/davila/propertymanagement/config/SwaggerConfig.java
+++ src/main/java/com/davila/propertymanagement/config/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package com.davila.propertymanagement.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,9 +15,8 @@ public class SwaggerConfig implements WebMvcConfigurer {
 
     @Bean
     public Docket appApi(){
-        Docket docket = new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.davila.propertymanagement.controller"))
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select().apis(RequestHandlerSelectors.basePackage("com.davila.propertymanagement.controller"))
                 .paths((PathSelectors.regex("/."))).build();
-        return docket;
     }
 }
